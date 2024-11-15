6342aefa6010b1b00ea7e06becf03a9e11d60d90
phounexxx <denissotovggg223@gmail.com>
2024-11-15 17:55:33 +0300

    added 4

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,7 +1,8 @@
-class Main{
+class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке dev");
         System.out.println("Мои попытки сделать что-то значимое би лайк");
         System.out.println("бебебе бубубу пам пам пум");
+        System.out.println(1 + 2);
     }
 }

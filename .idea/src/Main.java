a640126e24b0ae37c0d431a9d58b0c4d23011b56
phounexxx <denissotovggg223@gmail.com>
2024-11-15 17:50:36 +0300

    added 4

--- .idea/src/Main.java
+++ .idea/src/Main.java
@@ -1,9 +1,13 @@
 
 
-class Main{
+class Main {
     public static void main(String[] args) {
         System.out.print("Это добавлено в ветке main");
         System.out.println("это попытки внести в код что-то" +
                 "значимое");
+        int a = 2;
+        int b = 3;
+
+        System.out.println(a + b);
     }
 }
